f643fbc6e303456acf8d23069063a9423cf57a7f
Abdykali Azat <azat.abdykalli@gmail.com>
2024-08-02 11:41:03 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://cargo-almaty99.kz:3001',
+    
+    nameCargo: 'CARGO_ALMATY99'
+  };
+  
+  
+export default config;
+  
